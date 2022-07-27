c00e3198981b3a83f5c3dfdc08790783153ddcba
Barbearian <wouyang2@outlook.com>
2022-07-27 14:10:19 +0800

    updated to express server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-const io = require("socket.io");
-var lio = new io.Server(3000);
-lio.on("connection",(socket)=>{
-    console.log("Hello Bear");
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
