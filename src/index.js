a8120c75cf5a44a41451eff3da8d42c19ee2db4e
Barbearian <wouyang2@outlook.com>
2022-08-10 15:20:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,42 +5,40 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
- // res.sendFile(__dirname + '/index.html');
-});
+//app.get('/', (req, res) => {});
 
 //authenrication
 io.use((socket, next)=>{
   const userId = socket.handshake.auth.userId;
   const password = socket.handshake.auth.password;
   
-  console.log(userId +"logged in with password :"+password);
+  //a room for this user only
+  socket.join(userId);
+  console.log(userId +" tried to logged in with password :"+password);
   next();
 });
 
 io.on('connection', (socket) => {
-
-
-  console.log('a user connected');
-  io.emit("message",{id:socket.id, message:"I am connected"});
+  
+  const userid = socket.handshake.auth.userId;
+  console.log(userid+' is authorized');
+  io.emit("message",{id:userid, message:"has join the room"});
 
   socket.on("message",(msg)=>{
-    io.emit("message",{id:socket.id, message:msg});
-    console.log(msg);
+    io.emit("message",msg);
+    console.log(userid+": "+ msg);
   })
 
   socket.on("privateMessage",(key,message)=>{
-    socket.to(key).emit("message",message);
+    io.to(key).emit("message",message);
+    console.log(userid+"->"+key+": "+ msg);
   });
 
   socket.on("disconnect",()=>{
-    io.emit("message",{id:socket.id, message:"I am connected"});
+    io.emit("message",{id:userid, message:"is disconnected"});
     console.log(socket.id+" is disconnected");
   });
 });
-
-
-
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
