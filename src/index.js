acbfe9bdc6d1cea10d7645caeb0d3a079be58a3b
Barbearian <67714624+Barbearian@users.noreply.github.com>
2022-10-10 17:52:45 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -77,6 +77,6 @@ io.on('connection', (socket) => {
 
 
 
-server.listen(process.env.port, () => {
-  console.log('listening on *:'+process.env.port);
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
