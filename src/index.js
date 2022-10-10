aa26baeecc00697f8fc63d9ae75014e39427edbf
WenlongOuyang98 <111549554+BarbearianCN@users.noreply.github.com>
2022-10-10 15:21:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const axios = require("axios");
-const https = require('https');
-const { type } = require('os');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
@@ -75,6 +73,6 @@ io.on('connection', (socket) => {
 
 
 
-server.listen(443, () => {
-  console.log('listening on *:443');
+server.listen(80, () => {
+  console.log('listening on *:80');
 });
